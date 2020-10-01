9344c20b258ee3b8f2ab1c694028185c6707a594
skamok <sk_amok@mail.ru>
2020-10-01 23:04:40 +0300

    init: add solve for Towel Sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 
 // You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let dir = 'f';
+  let arr = []
+  if (matrix === undefined) return [];
+  if (matrix.length === 0) return [];
+  matrix.forEach(element => {
+    if (dir === 'f') {
+      arr = arr.concat(element);
+    } else {
+      arr = arr.concat(element.reverse());
+    }
+    
+    if (dir === 'f') {
+      dir = 'b'
+    } else {
+      dir = 'f'
+    }
+  });
+  return arr;
 }
+/*
+module.exports = function towelSort (matrix) {
+  if (matrix === undefined) return [];
+  if (Array.isArray(matrix)) {
+    return matrix.reduce( (accum, element) => {
+      return accum.concat(towelSort(element));
+    }, [])
+  } else {
+    return matrix;
+  }
+}*/
